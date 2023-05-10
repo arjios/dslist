86ff824f0ff4be0c91f8206c66509579250100f6
arjios <antonio.uab.ios.fisica@gmail.com>
2023-05-10 16:26:41 -0300

    Games FindById

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,19 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	public List<GameMinDTO> findAll() {
 		List<Game> list = gameRepository.findAll();
 		return list.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+	public GameDTO findById(Long id) {
+		Game obj = gameRepository.findById(id).get();
+		return new GameDTO(obj);
 	}
 
 }
