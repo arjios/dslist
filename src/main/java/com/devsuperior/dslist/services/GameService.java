e1d36edbe4638feec53e647a50b819bc99faa96b
arjios <antonio.uab.ios.fisica@gmail.com>
2023-05-10 17:19:13 -0300

    Game Lists

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
@@ -16,14 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> list = gameRepository.findAll();
 		return list.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
+	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
-		Game obj = gameRepository.findById(id).get();
-		return new GameDTO(obj);
+		return new GameDTO(gameRepository.findById(id).get());
 	}
 
 }
